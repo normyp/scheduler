f27e145d8c748212d3853743e85b0b668a0ddb40
Paul Normington <normyp1@yahoo.co.uk>
2015-01-02 13:50:20 +0000

    Swaps people round correctly but outputs doubles sometimes

--- output.cpp
+++ output.cpp
@@ -46,8 +46,8 @@ void output(gregorian::date startDay, gregorian::date endDay, int numberOfPeople
 	//need a timesmore than everyone else
 	int timesUsed;
 	int numberOfDrivers;
-	int count2;
-	bool check;
+	int extraPeopleCount;
+	bool check, first, changed;
 	string peopleOfDay[3];
 	string extraPeople[16];
 
@@ -55,12 +55,19 @@ void output(gregorian::date startDay, gregorian::date endDay, int numberOfPeople
 	//then I can use the vector of people to find the drivers and get rid of one of them for that day
 	//next time it happens this driver needs to replace the next driver
 	//
+	first = true;
 
 	for (gregorian::date i = startDay; i < endDay; i = i + dd)
 	{
+		changed = false;
 		cout << "The people working on " << i << " are as follows: " << endl;
-		count = 0;
-		count2 = 0;
+		if (first)
+		{
+			count = 0;
+			first = false;
+			extraPeopleCount = 0;
+		}
+	
 		numberOfDrivers = 0;
 		for (int j = 0; j < numberOfPeople; j++)
 		{
@@ -80,11 +87,11 @@ void output(gregorian::date startDay, gregorian::date endDay, int numberOfPeople
 				peopleOfDay[count] = people[j]->getName();
 				count++;
 			}
-			/*else if (personAvailable && count >= 3)
+			else if (personAvailable && count >= 3)
 			{
-				extraPeople[count2] = people[j]->getName();
-				count2++;
-			}*/
+				extraPeople[extraPeopleCount] = people[j]->getName();
+				extraPeopleCount++;
+			}
 			personAvailable = false;
 		}
 		if (count < 3)
@@ -97,24 +104,56 @@ void output(gregorian::date startDay, gregorian::date endDay, int numberOfPeople
 			{
 				cout << peopleOfDay[l] << endl;
 			}
+			cout << "Extra people are: ";
+			for (int l = 0; l < 16; l++)
+			{
+				if (extraPeople[l] != "")
+				{
+					cout << extraPeople[l] << " ";
+				}
+			}
+			cout << endl;
 		}
-		/*if (count2 > 0)
+		if (extraPeopleCount > 0)
 		{
-			if (count2 > 3)
+			if (extraPeopleCount > 3)
 			{
 				for (int y = 0; y < 3; y++)
 				{
 					peopleOfDay[y] = extraPeople[y];
+					extraPeople[y] = "";
 				}
+				for (int y = 0; y < 16; y++)
+				{
+					if (y + 3 < 16)
+					{
+						if (changed == false && extraPeople[y+3] == "")
+						{
+							extraPeopleCount = y;
+							changed = true;
+						}
+						extraPeople[y] = extraPeople[y + 3];
+					}
+				}
+				count = 3;
 			}
 			else{
-				for (int y = 0; y < count2; y++)
+				for (int y = 0; y < extraPeopleCount; y++)
 				{
 					peopleOfDay[y] = extraPeople[y];
+					extraPeople[y] = "";
+					count = extraPeopleCount;
 				}
-			}
-			count = count2;
-		}*/
+				for (int y = 0; y < 16; y++)
+				{
+					if (y + extraPeopleCount < 16)
+					{
+						extraPeople[y] = extraPeople[y + extraPeopleCount];
+					}
+				}
+				extraPeopleCount = 0;
+			}			
+		}
 		else
 		{
 			count = 0;
